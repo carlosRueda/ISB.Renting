1c1424c4a0daaa2a17fcc8b4143e60aa94788d1b
Carlos Rueda V <carlosnake.182@gmail.com>
2024-03-20 22:58:41 +0100

    re organize

---
+++ ISB.Renting.RentingUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ISB.Renting.RentingUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
